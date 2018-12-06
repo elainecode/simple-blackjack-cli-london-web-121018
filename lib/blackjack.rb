04c17ef2a42a4e6e5d448c26b1e7fc8754a09cdf
I_AM Elaine <elaine.onb@gmail.com>
2018-12-06 12:33:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,12 +1,14 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
+def display_card_total(num)
   # code #display_card_total here
 end
 
